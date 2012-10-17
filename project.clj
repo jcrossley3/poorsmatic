f82181c15318d43d074968ec7e00ad6269b793d2
Jim Crossley <jim@crossleys.org>
2012-10-17 18:55:38 -0400

    Interestingly, switching dep order also works, but not at runtime :(

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [twitter-api "0.6.11" :exclusions [org.apache.httpcomponents/httpcore]]
-                 [clj-http "0.5.5"]]
+                 [clj-http "0.5.5"]
+                 [twitter-api "0.6.11"]]
   :immutant {:swank-port 4005})

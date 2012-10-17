237f6fc35a23dd2fe524643fe77d87f0fe675c36
Jim Crossley <jim@crossleys.org>
2012-10-17 14:19:06 -0400

    To the REPL!

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject poorsmatic "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A poor man's Prismatic"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :immutant {:swank-port 4005})

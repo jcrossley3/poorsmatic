cc6788edffa65776d8541f900c696b123fb81833
Jim Crossley <jim@crossleys.org>
2012-10-20 23:04:26 -0400

    Minor

--- src/poorsmatic/scrape.clj
+++ src/poorsmatic/scrape.clj
@@ -6,7 +6,8 @@
             [immutant.messaging :as msg]))
 
 (defn make-scraper
-  "Returns a function that, given a url, counts the number of words in its content"
+  "Returns a function that, given a url, counts the number of words in
+   its content, among other things"
   [word]
   (let [scrape (comp (counter word) title url scrape)]
     (fn [url]
@@ -17,19 +18,22 @@
         v))))
 
 (defn make-scrapers
+  "Make scrapers for a sequence of words"
   [words]
   (if (empty? words)
     (fn [x])
     (apply juxt (map make-scraper words))))
 
 (defn start
+  "Scrape urls looking for words received from the configuration topic"
   [endpoint]
   (let [scraper (atom (fn [x]))
-        listener (msg/listen endpoint (fn [url] (@scraper url)) :concurrency 10)
+        listener (msg/listen endpoint (fn [url] (@scraper url)))
         config (cfg/observe #(reset! scraper (make-scrapers %)))]
     [listener config]))
 
 (defn stop
+  "Cleanly shutdown the return value of start"
   [[listener config]]
   (msg/unlisten listener)
   (cfg/ignore config))

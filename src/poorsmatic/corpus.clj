67a5a9755fad989ddab76a21b21fe429641295e2
Jim Crossley <jim@crossleys.org>
2012-10-23 17:52:41 -0400

    Rename and add enough methods to drop-in for clj-http and twitter-api

--- src/poorsmatic/corpus.clj
+++ src/poorsmatic/corpus.clj
@@ -1,9 +1,27 @@
-(ns poorsmatic.corpuscle
+(ns poorsmatic.corpus
+  (:refer-clojure :exclude (get))
   (:require [clojure.java.io :as io]
             [poorsmatic.twitter :as twitter]
             [poorsmatic.tweets :as tweets]
             [poorsmatic.http :as http]
             [immutant.messaging :as msg]))
+
+(def corpus-path "/tmp/corpus")
+
+(defn filter-tweets
+  [filter callback]
+  (future (doseq [f (sort (.listFiles (io/file corpus-path "tweets")))]
+            (Thread/sleep (rand-int 500))
+            (callback (read-string (slurp f))))))
+
+(defn close
+  [stream]
+  (future-cancel stream))
+
+(defn get
+  [url options]
+  (Thread/sleep (+ 500 (rand-int 1000)))
+  (read-string (slurp (io/file corpus-path "urls" (clj-http.util/url-decode url)))))
 
 (defn tweet-saver
   [path]

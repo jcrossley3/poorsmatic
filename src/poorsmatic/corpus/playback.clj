c75ea07a01c7fe72bb7e3da6285ec39dbcf98a26
Jim Crossley <jim@crossleys.org>
2012-10-24 19:51:32 -0400

    More realistic playback, not starting over every time and honoring timestamps

--- src/poorsmatic/corpus/playback.clj
+++ src/poorsmatic/corpus/playback.clj
@@ -2,9 +2,13 @@
   (:refer-clojure :exclude (get))
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clj-http.util :as util]))
+            [clj-http.util :as util]
+            [clj-time.core :as t]
+            [clj-time.format :as tf]))
 
 (def corpus-path "/tmp/corpus")
+(def tweet-files (atom (cycle (.listFiles (io/file corpus-path "tweets")))))
+(def formatter (tf/formatter "EEE MMM dd HH:mm:ss Z yyyy"))
 
 (defn filter-tweets
   [filter callback]
@@ -12,12 +16,19 @@
     (let [terms (str/split filter #",")
           done (atom false)]
       (with-meta
-        (future (loop [files (cycle (.listFiles (io/file corpus-path "tweets")))]
-                  (let [tweet (read-string (slurp (first files)))]
-                    (when (some #(re-find (re-pattern (str "(?i)\\b" % "\\b")) (:text tweet)) terms)
-                      (callback tweet)
-                      (Thread/sleep (rand-int 1000)))
-                    (if-not @done (recur (rest files))))))
+        (future (loop [files @tweet-files
+                       n1 (t/now)
+                       t1 n1]
+                  (let [tweet (read-string (slurp (first files)))
+                        t2 (tf/parse formatter (:created_at tweet))]
+                    (if (or (t/after? t1 t2) (t/after? (t/now) (t/plus n1 (t/secs (t/in-secs (t/interval t1 t2))))))
+                      (do
+                        (if (some #(re-find (re-pattern (str "(?i)\\b" % "\\b")) (:text tweet)) terms)
+                          (callback tweet))
+                        (recur (swap! tweet-files rest) (t/now) t2))
+                      (do
+                        (Thread/sleep 500)
+                        (if-not @done (recur files n1 t1)))))))
         {:done done}))))
 
 (defn close

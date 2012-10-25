36d07de9a19f87f2269916b200af5f10f8c8b54e
Tobias Crawley <tcrawley@gmail.com>
2012-10-25 13:41:13 -0400

    Simplify.

--- src/poorsmatic/models.clj
+++ src/poorsmatic/models.clj
@@ -2,29 +2,15 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]
             [immutant.utilities :as util])
-  (:use [datomic.api :only (q db) :as d]))
+  (:use [datomic.api :only (q db transact) :as d]))
 
 ;; mem uri: "datomic:mem://poorsmatic"
 (let [uri "datomic:free://localhost:4334/poorsmatic"]
   (d/create-database uri)
   (defonce conn (d/connect uri)))
 
-(defn tx [body]
-  (d/transact conn body))
-
 (defn setup-db []
-  (tx (read-string (slurp (io/resource "schema.dtm")))))
-
-(defn q1 [query & args]
-  (ffirst (apply q query (db conn) args)))
-
-(defn qe [query & args]
-  (d/entity (db conn) (apply q1 query args)))
-
-(defn qes [query & args]
-  (let [db (db conn)]
-    (->> (apply q query db args)
-         (mapv (fn [vals] (mapv (partial d/entity db) vals))))))
+  (transact conn (read-string (slurp (io/resource "schema.dtm")))))
 
 (defn add-term  [term]
   (d/transact
@@ -34,17 +20,18 @@
 
 (defn delete-term
   [term]
-  (if-let [t (qe '[:find ?e :in $ ?term :where [?e :tweet/term ?term]]
-                 (str/lower-case term))]
-    (tx [[:db.fn/retractEntity (:db/id t)]])))
+  (if-let [tid (ffirst (q '[:find ?e :in $ ?term :where
+                            [?e :tweet/term ?term]]
+                          (db conn)
+                          (str/lower-case term)))]
+    (transact conn [[:db.fn/retractEntity tid]])))
 
 (defn get-all-terms
   []
-  (let [db (db conn)]
-    (->> (qes '[:find ?e :in $ :where [?e :tweet/term]])
-         (map first)
-         (sort-by :db/id)
-         (map :tweet/term))))
+  (->> (q '[:find ?term ?t :in $ :where
+            [?t :tweet/term ?term]] (db conn))
+       (sort-by last)
+       (map first)))
 
 (defn add-url
   [{:keys [term url title count]}]
@@ -52,14 +39,18 @@
         base-url {:db/id #db/id [:db.part/user]
                   :url/term count-id
                   :url/url url}]
-    (tx [{:db/id count-id
-           :term-count/term (str/lower-case term)
-           :term-count/count count}
-          (if-let [url-id (q1 '[:find ?id :in $ ?url :where [?id :url/url ?url]] url)]
-            {:db/id url-id :url/term count-id}
-            (if title
-              (assoc base-url :url/title title)
-              base-url))])))
+    (transact
+     conn
+     [{:db/id count-id
+       :term-count/term (str/lower-case term)
+       :term-count/count count}
+      (if-let [url-id (ffirst
+                       (q '[:find ?e :in $ ?url :where [?e :url/url ?url]]
+                          (db conn) url))]
+        {:db/id url-id :url/term count-id}
+        (if title
+          (assoc base-url :url/title title)
+          base-url))])))
 
 (defn find-urls-by-term
   [term]
@@ -71,7 +62,7 @@
             [?u :url/url ?url]
             [?u :url/title ?title]
             [?t :term-count/count ?count]]
-          (db conn)          
+          (db conn)
           (str/lower-case term))
        (sort-by #(nth % 2) >)
        (take 10)))

509f885c222ac91a9b647f0d91404e82a88e7f0a
Jim Crossley <jim@crossleys.org>
2013-10-23 20:32:32 -0400

    Default to using Immutant as Datomic's data store

--- project.clj
+++ project.clj
@@ -3,15 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [clj-http "0.5.5"]
-                 [com.datomic/datomic-free "0.8.3551"
-                  :exclusions [postgresql]]
+                 [org.infinispan/infinispan-client-hotrod "6.0.0.CR1"]
+                 [com.datomic/datomic-pro "0.8.4159"]
                  [twitter-api "0.6.12"]
                  [compojure "1.1.3"]
                  [hiccup "1.0.1"]]
-  :profiles {:dev {:immutant {:swank-port 4005}
-                   :datomic-uri "datomic:mem://poorsmatic"}
-             :prod {:immutant {:init poorsmatic.core/start}
-                    :datomic-uri "datomic:free://localhost:4334/poorsmatic"}})
+  :profiles {:prod {:immutant {:init poorsmatic.core/start}}}
+  :datomic-uri "datomic:inf://localhost:11222/poorsmatic")
